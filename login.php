bc623d4937000403c14ce7f38d41471ab2fe9fb3
CameronCod <142741540+CameronCod@users.noreply.github.com>
2025-09-07 20:27:19 +0200

    Update login.php

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - EduMate</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
@@ -100,4 +100,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         });
     </script>
 </body>
+
 </html>
