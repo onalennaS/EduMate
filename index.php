86ca106e9540f6e4e46fb14d9466ec5898f093c3
CameronCod <142741540+CameronCod@users.noreply.github.com>
2025-09-07 22:12:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'includes/auth.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EduMate - Accessible Science Education</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="assets/css/dashboard.css">
 </head>
 <body>
